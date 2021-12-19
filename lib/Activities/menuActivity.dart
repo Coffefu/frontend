e25678564fad49003ff7a63006b3c8ed481ac57e
Karim <karim.idrisow2016@gmail.com>
2021-12-20 09:08:04 +1000

    base layout

---
+++ lib/Activities/menuActivity.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MenuActivity extends StatefulWidget {
+  const MenuActivity({Key? key}) : super(key: key);
+
+  @override
+  State<MenuActivity> createState() => _MenuActivityState();
+}
+
+class _MenuActivityState extends State<MenuActivity> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/bottomNavigation.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/Activities/cartActivity.dart';
+import 'package:frontend/Activities/menuActivity.dart';
+import 'package:frontend/Activities/profileActivity.dart';
+
+class BottomNavigationWidget extends StatefulWidget {
+  const BottomNavigationWidget({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavigationWidget> createState() => _BottomNavigationWidgetState();
+}
+
+class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    MenuActivity(),
+    CartActivity(),
+    ProfileActivity(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Coffefu"),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.emoji_food_beverage),
+            label: 'МЕНЮ',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'КОРЗИНА',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'ПРОФИЛЬ',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
