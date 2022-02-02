e36d245cc5718f1fbf4774fbe612579c40f160db
hedgehog319 <fign1910@mail.ru>
2022-02-02 13:59:27 +1000

    + Add InkWell

--- lib/components/product.dart
+++ lib/components/product.dart
@@ -8,29 +8,34 @@ class Product {
   Product(this.id, this.name, this.imgUrl);
 }
 
-class ProductWidget extends StatelessWidget {
+class ProductWidget extends StatefulWidget {
   late Product product;
 
   ProductWidget(this.product, {Key? key}) : super(key: key);
 
   @override
+  State<ProductWidget> createState() => _ProductWidgetState();
+}
+
+class _ProductWidgetState extends State<ProductWidget> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.fromLTRB(27, 30, 27, 17),
-          child: Image.network(product.imgUrl),
-        ),
-        Text(product.name)
-      ]),
-      decoration: BoxDecoration(
+    return Material(
         color: const Color(0xFFF7F8FB),
+        elevation: 8,
         borderRadius: BorderRadius.circular(15),
-        boxShadow: const [
-          BoxShadow(
-              color: Color(0x4F000000), blurRadius: 4, offset: Offset(0, 4))
-        ],
-      ),
-    );
+        child: InkWell(
+            borderRadius: BorderRadius.circular(15),
+            onTap: () => {}, //TODO implement Product click method
+            child: Container(
+              padding: const EdgeInsets.all(0.0),
+              child: Column(children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(27, 30, 27, 17),
+                  child: Image.network(widget.product.imgUrl),
+                ),
+                Text(widget.product.name)
+              ]),
+            )));
   }
 }
