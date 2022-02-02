6945a466273998fe131e2a38c09070a703715fbc
hedgehog319 <fign1910@mail.ru>
2022-02-02 14:01:11 +1000

    - Removed fetching products from the server

--- lib/Activities/menu_activity.dart
+++ lib/Activities/menu_activity.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:frontend/components/product.dart';
-import "package:http/http.dart" as http;
+import 'package:frontend/store/store.dart' as store;
 
 class MenuActivity extends StatefulWidget {
   const MenuActivity({Key? key}) : super(key: key);
@@ -12,29 +10,13 @@ class MenuActivity extends StatefulWidget {
 }
 
 class _MenuActivityState extends State<MenuActivity> {
-  List<ProductWidget> products = [];
-
-  // REDO fetchData()
-  void fetchData() async {
-    try {
-      final response =
-          await http.get(Uri.parse('http://10.0.2.2:8000/products'));
-
-      var data = jsonDecode(response.body) as List;
-
-      var products = List<ProductWidget>.generate(data.length, (i) {
-        var d = data[i];
-        return ProductWidget(Product(d['id'], d['name'], d['img']));
-      });
-      setState(() {
-        this.products = products;
-      });
-    } catch (e) {}
-  }
-
   @override
   Widget build(BuildContext context) {
-    fetchData();
+    var products = List<ProductWidget>.generate(store.productsList.length, (i) {
+      var d = store.productsList[i];
+      return ProductWidget(Product(d['id'], d['name'], d['img']));
+    });
+
     return GridView.count(
       padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
       crossAxisCount: 2,
