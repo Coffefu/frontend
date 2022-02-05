fa0e66b32d709b7984ae8b797d2548fc010c3b1b
hedgehog319 <fign1910@mail.ru>
2022-02-05 11:58:14 +1000

    * Fix LoadActivity background

--- lib/Activities/load_activity.dart
+++ lib/Activities/load_activity.dart
@@ -11,31 +11,33 @@ class LoadActivity extends StatefulWidget {
 class _LoadActivityWidgetState extends State<LoadActivity> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
+    return Material(
+        color: Colors.white,
+        child: Center(
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text("Cofefu", style: TextStyle(fontSize: 40)),
-              // REDO use SvgPicture.assets
-              SvgPicture.network('http://10.0.2.2:8000/favicon.svg')
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Cofefu", style: TextStyle(fontSize: 40)),
+                  // REDO use SvgPicture.assets
+                  SvgPicture.network('http://10.0.2.2:8000/favicon.svg')
+                ],
+              ),
+              const SizedBox(height: 10),
+              const SizedBox(
+                width: 100,
+                height: 100,
+                child: CircularProgressIndicator(
+                  strokeWidth: 10,
+                  color: Color(0xFF7D5DFF),
+                ),
+              ),
             ],
           ),
-          const SizedBox(height: 10),
-          const SizedBox(
-            width: 100,
-            height: 100,
-            child: CircularProgressIndicator(
-              strokeWidth: 10,
-              color: Color(0xFF7D5DFF),
-            ),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
