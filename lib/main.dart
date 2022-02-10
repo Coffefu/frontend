529053be33cc09354abcb3cbc2db06bf512dbd62
hedgehog319 <fign1910@mail.ru>
2022-02-10 19:12:05 +1000

    + Routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:frontend/activities/load_activity.dart';
+import 'package:frontend/activities/order_activity.dart';
+import 'package:frontend/common/components/product.dart';
 import 'package:frontend/common/models/app_state.dart';
 import 'package:frontend/store/reducers/app_reducers.dart';
 import 'package:redux/redux.dart';
@@ -9,7 +12,6 @@ import 'common/models/menu.dart';
 import 'navigation.dart';
 
 void main() {
-
   final store = Store<AppState>(
     appStateReducer,
     initialState: AppState([], Menu([])),
@@ -28,8 +30,23 @@ class CoffeeApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return StoreProvider(
         store: store,
-        child: const MaterialApp(
-          home: NavigationWidget()
+        child: MaterialApp(
+          initialRoute: '/',
+          routes: {
+            '/': (BuildContext context) => const NavigationWidget(),
+            '/loadScree': (BuildContext context) => const LoadActivity(),
+          },
+          onGenerateRoute: (settings) {
+            if (settings.name == '/orderActivity') {
+              return MaterialPageRoute(builder: (context) {
+                return OrderActivity(
+                    product: settings.arguments as ProductCard);
+              });
+            }
+
+            assert(false, 'Need to implement ${settings.name}');
+            return null;
+          },
         ));
   }
 }
