9dd4e562597c3c941ca040c99811f5ac321f9915
hedgehog319 <fign1910@mail.ru>
2022-02-27 13:33:00 +1000

    * Update Product Models

--- lib/common/models/product_model.dart
+++ lib/common/models/product_model.dart
@@ -1,18 +1,26 @@
 class Product {
-  String name;
-  String description;
-  int price;
+  final String name;
+  final String description;
+  final int price;
 
   Product({required this.name, required this.description, required this.price});
+
+  Product.clone({required Product product})
+      : name = product.name,
+        description = product.description,
+        price = product.price;
 }
 
-// TODO cardProduct implementation
 class CardProduct extends Product {
   int count;
 
-  CardProduct({required this.count, required Product product})
-      : super(
-            name: product.name,
-            description: product.description,
-            price: product.price);
+  CardProduct(
+      {required String name,
+      required String description,
+      required int price,
+      required this.count})
+      : super(name: name, description: description, price: price);
+
+  CardProduct.fromProduct({required Product product, required this.count})
+      : super.clone(product: product);
 }
