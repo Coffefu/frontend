27e027d78c65010c44bdb979c5cdcabadad6986a
hedgehog319 <fign1910@mail.ru>
2022-02-27 12:56:24 +1000

    * A small extension of the ProductModel

--- lib/common/models/product_model.dart
+++ lib/common/models/product_model.dart
@@ -1,6 +1,18 @@
 class Product {
   String name;
   String description;
+  int price;
 
-  Product(this.name, this.description);
+  Product({required this.name, required this.description, required this.price});
+}
+
+// TODO cardProduct implementation
+class CardProduct extends Product {
+  int count;
+
+  CardProduct({required this.count, required Product product})
+      : super(
+            name: product.name,
+            description: product.description,
+            price: product.price);
 }
