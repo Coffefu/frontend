558f1b3f4330956df3139a8ee9468b9a7a748b35
hedgehog319 <fign1910@mail.ru>
2022-02-27 12:59:16 +1000

    - A little optimization

--- lib/store/thunk/fetch_menu.dart
+++ lib/store/thunk/fetch_menu.dart
@@ -1,25 +1,27 @@
-import 'package:frontend/common/components/product.dart';
+import 'dart:convert';
+
 import 'package:frontend/common/models/app_state.dart';
 import 'package:frontend/common/models/product_model.dart';
 import 'package:frontend/store/actions/menu_actions.dart';
-import 'package:redux_thunk/redux_thunk.dart';
-import 'package:redux/redux.dart';
-import 'dart:convert';
 import "package:http/http.dart" as http;
+import 'package:redux/redux.dart';
+import 'package:redux_thunk/redux_thunk.dart';
 
 ThunkAction<AppState> getMenuProducts() {
   return (Store<AppState> store) async {
     var productsList = [];
     final response = await http.get(
-          Uri.parse('http://51.250.4.2/products'),
-          headers: {'Content-Type': 'application/json'});
+      Uri.parse('http://51.250.37.59/products'),
+      headers: {'Content-Type': 'application/json'},
+    );
 
-    productsList =
-          List.from(jsonDecode(utf8.decode(response.bodyBytes)));
-    var products = <Product>[];
-    for (var i = 0; i < productsList.length; i++) {
-      products.add(Product(productsList[i]['name'], ''));
-    }
+    productsList = List.from(jsonDecode(utf8.decode(response.bodyBytes)));
+    var products = List<Product>.generate(
+        productsList.length,
+        (i) => Product(
+            name: productsList[i]['name'],
+            description: productsList[i]['description'] ?? "",
+            price: 0));
     if (products.isNotEmpty) {
       store.dispatch(menuLoadingSuccessAction(products));
     } else {
